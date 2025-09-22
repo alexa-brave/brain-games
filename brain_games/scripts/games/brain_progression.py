145d60e21974e03b6daa7a1e36f52de6c13c3443
alexa-brave <merkel_795@mail.ru>
2025-09-22 21:39:54 +0300

    feat: прописана большая часть логики для brain-progression

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -12,7 +12,24 @@ from brain_games.scripts.games_logic import question
 def rules_the_game():  # объясняем правила конкретной мини игры
     print('What number is missing in the progression?')
 
+# ошибка в цикле, list.remove(x): x not in list
+
 def is_progression_game():  # выбор неизвестных для одного раунда игры
+    len_of_progression: int = randint(5, 15)
+    start_num: int = randint(1, 100)
+    step_of_progression: int = randint(1, 9)
+    position_unknown: int = randint(1, len_of_progression) - 1
+    # последовательность
+    subsequence: list = [start_num]
+    for i in list(range(len_of_progression + 1)):
+        new_element: int = subsequence[-1] + step_of_progression
+        subsequence.append(new_element)
+        # remove(x)
+        # pop([i])
+    correct_answer = subsequence[position_unknown]
+    subsequence.remove(position_unknown)
+    unknown: list = subsequence.insert(position_unknown - 1, '..')
+    print(subsequence)
     return unknown, correct_answer
 
 

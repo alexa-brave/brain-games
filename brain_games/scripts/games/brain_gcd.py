d9dd31bb207b500e2b620dff3d825c246c5cdf57
alexa-brave <merkel_795@mail.ru>
2025-09-22 14:52:02 +0300

    fix: объединила функцию логики конкретной мини игры в одну для корректной работы внутри общей логики программы

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -1,23 +1,21 @@
 from random import randint  # для случайного выбора чисел
 import math
-import sys
+# import sys
 from brain_games.scripts.games_logic import question
 
 # пользователю показывается два случайных числа, например,
 # 25 50. Пользователь должен вычислить и ввести наибольший
 # общий делитель этих чисел.
 
-# Реализуйте необходимую логику для работы игры
-# Проверьте работоспособность новой игры
 # Добавьте в README.md аскинему с запуском и демонстрацией различных исходов игры
 
 def rules_the_game():  # объясняем правила игры
     print('Find the greatest common divisor of given numbers.')
 
-def unknows():  # поиск неизвестных
+def gcd_game():  # поиск неизвестных
     # рандомизация неизвестных мини-игры
     num_1: int = randint(-100, 100)
-    num_2 = 0
+    num_2: int = 0  # задаём стартовое значение для цикла
     # проверка, чтобы оба числа не были нулями одновременно
     if num_1 == 0:
         while num_2 == 0:
@@ -25,23 +23,19 @@ def unknows():  # поиск неизвестных
     else:
         num_2: int = randint(-100, 100)
         print(num_1, num_2)
-    return num_1, num_2
-
-def correct_answer():
-    num_1, num_2 = unknows()  # разбиваем кортеж на переменные
-    correct_answer = math.gcd(num_1, num_2)  # выясняем правильный ответ
-    return correct_answer
-
-def brain_gcd():
-    a = unknows()
-    b = correct_answer()
-    print(a, b)
+    # выясняем правильный ответ, используя функцию для поиска НОД,
+    # делаем результат строкой, тк ввод игрока будет в виде строки,
+    # а его ввод будет сравниваться с правильным ответом
+    correct_answer: str = str(math.gcd(num_1, num_2))
+    # делаем результат строкой для стандартизации аргументов в функции логики вопросов, чтобы 2 числа были 1 аргументом
+    result: str = f'{num_1} {num_2}'
+    return result, correct_answer
 
 
 def main():  # точка входа для мини-игры brain-gcd
     # запускаем универсальную для всех мини-игр функцию
     # даём ей на вход неизвестное и правильный ответ для этой мини-игры
-    question(brain_gcd, rules_the_game)
+    question(gcd_game, rules_the_game)
 
 if __name__ == '__main__':
     main()

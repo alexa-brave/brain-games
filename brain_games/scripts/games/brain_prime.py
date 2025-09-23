379d2c0545a155915c941b8eafc82c183239da15
alexa-brave <merkel_795@mail.ru>
2025-09-23 18:16:47 +0300

    feat: готова логика мини-игры brain-prime

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -1,13 +1,27 @@
 from brain_games.scripts.games_logic import question
-
+from random import randint
 
 # Игра «Простое ли число?»
+
+# Функцию проверки является ли число простым, нужно реализовать самостоятельно, без библиотек
+
+# To do:
+# Добавьте в README.md аскинему с запуском и демонстрацией различных исходов игры
 
 def rules_the_game():  # объясняем правила конкретной мини игры
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
 def is_prime_game():
-    # main_skript
+    simple_nums: list = [2, 3, 5, 7, 11, 13, 17, 
+                        19, 23, 29, 31, 37, 41, 
+                        43, 47, 53, 59, 61, 67,
+                        71, 73, 79, 83, 89, 97]
+    unknown: int = randint(0, 100)
+    if unknown in simple_nums:
+        correct_answer: str = 'yes'
+    else:
+        correct_answer = 'no'
+    return unknown, correct_answer
 
 def main():  # точка входа для мини-игры
     # запускаем универсальную для всех мини-игр функцию

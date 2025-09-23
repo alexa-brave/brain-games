95ab9e5606afb02c9c33971b198fbdc60481b660
alexa-brave <merkel_795@mail.ru>
2025-09-23 16:10:56 +0300

    feat: закончена логика мини игры gcd

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -1,5 +1,4 @@
 from random import randint
-from random import choice
 from brain_games.scripts.games_logic import question
 # Показываем игроку ряд чисел, который образует арифметическую прогрессию,
 # заменив любое из чисел двумя точками. Игрок должен определить это число.
@@ -12,25 +11,26 @@ from brain_games.scripts.games_logic import question
 def rules_the_game():  # объясняем правила конкретной мини игры
     print('What number is missing in the progression?')
 
-# ошибка в цикле, list.remove(x): x not in list
 
 def is_progression_game():  # выбор неизвестных для одного раунда игры
+    # задаём длину прогрессии в рамках рекомендуемой длины
     len_of_progression: int = randint(5, 15)
-    start_num: int = randint(1, 100)
-    step_of_progression: int = randint(1, 9)
+    start_num: int = randint(1, 100)  # задаём стартовое число
+    second_num: int = randint(start_num + 1, 100)  # задаём второе число для старта прогрессии
+    subsequence: list = [start_num, second_num]  # последовательность
+
+    # выясняем оставшиеся числа прогрессии
+    for i in list(range(len_of_progression - 2)):  # длина прогрессии минус два первоначально известных элемента
+        new_element: int = subsequence[-2] + subsequence[-1]  # сумма двух последних элементов прогрессии
+        subsequence.append(new_element)  # добавляем новый элемент в список прогрессии
+    
+    # задаём позицию неизвестного числа
     position_unknown: int = randint(1, len_of_progression) - 1
-    # последовательность
-    subsequence: list = [start_num]
-    for i in list(range(len_of_progression + 1)):
-        new_element: int = subsequence[-1] + step_of_progression
-        subsequence.append(new_element)
-        # remove(x)
-        # pop([i])
-    correct_answer = subsequence[position_unknown]
-    subsequence.remove(position_unknown)
-    unknown: list = subsequence.insert(position_unknown - 1, '..')
-    print(subsequence)
-    return unknown, correct_answer
+    correct_answer = subsequence[position_unknown]  # правильный ответ для сравнения со вводом игрока
+    subsequence.remove(correct_answer)  #  удаляем неизвестное из списка прогрессии
+    subsequence.insert(position_unknown, '..')  # ставим вместо неизвестного '..'
+    unknown: list = subsequence  # последовательность с пропущеным неизвестным
+    return unknown, str(correct_answer)
 
 
 def main():  # точка входа для мини-игры
